8806af177b8cfef06b0fccc5bdf77301b7d9ad3d
Larry Lai <cgpanda.sg@gmail.com>
2023-05-23 18:02:15 +0800

    use local server

--- expo-updates-client/App.js
+++ expo-updates-client/App.js
@@ -29,6 +29,7 @@ export default function App() {
       <Text>Larry Test 2023-05-19 16:19</Text>
       <Text>{Constants.expoConfig.name}</Text>
       <Text>{Constants.expoConfig.version}</Text>
+      <Text>{Constants.expoConfig.updates.url}</Text>
       <Image source={require('./assets/favicon.png')} />
       <Button title="Fetch update" onPress={onFetchUpdateAsync} />
       <StatusBar style="auto" />
